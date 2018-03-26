0d6154dbdfa97c28055a1258553cef2cd2fefc99
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-26 11:30:05 +0100

    Update user stories

---
+++ README.md
@@ -0,0 +1,23 @@
+## Bank Tech Test
+
+#### User stories
+
+```
+As a customer
+So I can store my money
+I want to have an account with a balance
+
+As a customer
+So I can save money
+I want to be able to deposit money
+
+As a customer
+So I can spend money
+I want to be able to withdraw money
+
+As a customer
+So I can see my account's history
+I want to view my statement
+
+
+```
--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,12 +1,12 @@
-describe("Account", () => {
+describe('Account', () => {
   it('accepts an initial balance', () => {
-    let account = new Account(100);
+    const account = new Account(100);
 
     expect(account.balance).toEqual(100);
   });
 
   it('has initial balance 0 if none is given', () => {
-    let account = new Account();
+    const account = new Account();
 
     expect(account.balance).toEqual(0);
   });
